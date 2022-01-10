68dc2ef7ec10124a90c0ebd1019e8feb6a9f9ec5
Nikolay Tonev <nikolay.tonev@gmail.com>
2022-01-10 15:01:09 +0100

    removing set ia cookie

--- ivmsesman.go
+++ ivmsesman.go
@@ -421,8 +421,6 @@ func (sm *Sesman) SessionAuth(w http.ResponseWriter, r *http.Request, at, rt str
 		return fmt.Errorf("error updating Authed session: %s", err.Error())
 	}
 
-	w.Header().Add("Set-Cookie", "ia=1; HttpOnly")
-
 	nsCookie := http.Cookie{
 		Name:     sm.cfg.CookieName,
 		Value:    url.QueryEscape(nsid),
